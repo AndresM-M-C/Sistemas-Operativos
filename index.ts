30fc7f7668cef4dca9252442acb5b2f7abbec150
AndresM-M-C <martinezcruzmanuel123@gmail.com>
2026-08-16 19:40:59 -0600

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,21 +1,6 @@
-console.log("prueba")
-import mongoose from "mongoose";
+
+
 import express from "express";
-import cors from "cors";
-import todoRoutes from "./todo.routes";
+import mongoose from "mongoose";
 
-const app = express();
-app.use(cors());
-app.use(express.json())
-app.use("/pendientes", todoRoutes)
-
-
-
-mongoose.connect("mongodb://localhost:27017/")
-    .then(() => {
-        app.listen(3000, () =>
-            console.log("Conectado y corriendo en el puerto http://localhost:3000"));
-    }).catch(error => console.log("error: " + error))
-
-
-
+const app get.productos
